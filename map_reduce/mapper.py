2e14ae5adb0cdde69ab840a2b4a77ffa8b2b95a9
Amulya <vedamy819@gmail.com>
2022-02-16 22:56:10 -0600

    commit

--- map_reduce/mapper.py
+++ map_reduce/mapper.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
-
-# import sys because we need to read and write data to STDIN and STDOUT
 import sys
+import re
 
-# reading entire line from STDIN (standard input)
 for line in sys.stdin:
-	# to remove leading and trailing whitespace
-	line = line.strip()
-	# split the line into words
-	words = line.split()
-	
-	# we are looping over the words array and printing the word
-	# with the count of 1 to the STDOUT
+	#remove leading and trailing whitespaces
+	line=line.strip()
+	#split words into lines
+	words=line.split()
+
 	for word in words:
-		print('%s\t%s' % (word, 1))
+		word=re.findall(r"[\w]+|[^\s\w]", word)
+		for word in word:
+			if word=='':
+				pass
+			else:
+				print(word.lower() + "\t" + '1')
+				
--- map_reduce/reducer.py
+++ map_reduce/reducer.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
 
-# import sys because we need to read and write data to STDIN and STDOUT
-import string
 import sys
 
-# reading entire line from STDIN (standard input)
+current_word = None
+current_count = 0
+word = None
+
+# read the entire line from STDIN
+
 for line in sys.stdin:
-	# to remove leading and trailing whitespace
+	# remove leading and trailing whitespace
 	line = line.strip()
-	# Convert the characters in line to lowercase to avoid case mismatch
-	line = line.lower()
-	# Remove the punctuation marks from the line
-	new_line = line.translate(line.maketrans("", "", string.punctuation))
-	# split the line into words
-	words = new_line.split()
-	for i in line:
-		if i in string.punctuation:
-			print(i)
-			# we are looping over the words array and printing the word
-			# with the count of 1 to the STDOUT
-			for word in words:
-				# write the results to STDOUT (standard output);
-				# what we output here will be the input for the
-				# Reduce step, i.e. the input for reducer.py
-				print('%s\t%s' % (word, 1))
+    # splitting the data on the basis of tab we have provided in mapper.py
+	word, count = line.split('\t', 1)
+	count = int(count)
+	if current_word == word:
+		current_count += count
+		
+	else:
+		if current_word:
+			print(str(current_count)+ "\t" + current_word)
+		current_count = count
+		current_word = word
+
+				
+if current_word == word:
+	print(str(current_count)+ "\t" + current_word)
